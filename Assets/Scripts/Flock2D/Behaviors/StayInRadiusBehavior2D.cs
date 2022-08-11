bad7163d2cc2ad1e10e128f800e2b813e03b7a76
Charly <charly.mourglia@gmail.com>
2022-08-12 00:25:30 +0200

    Keep agents close to the center

---
+++ Assets/Scripts/Flock2D/Behaviors/StayInRadiusBehavior2D.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock2D/Stay in radius behavior")]
+public class StayInRadiusBehavior2D : FlockBehavior2D
+{
+    [SerializeField] Vector2 center;
+    [SerializeField] float radius = 15f;
+
+    public override Vector2 ComputeMove(FlockAgent2D agent, List<Transform> neighbors, Flock2D flock)
+    {
+        Vector2 centerOffset = center - (Vector2)agent.transform.position;
+
+        float t = centerOffset.magnitude / radius;
+
+        if (t < 0.9f)
+        {
+            // We are not too far from the center
+            return Vector2.zero;
+        }
+
+        return centerOffset * t * t;
+    }
+}
